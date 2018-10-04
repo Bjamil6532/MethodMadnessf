3063e992d19230402058ae81c9f7f12c6395e449
BT_1N3_13 <bjamil6532@bths.edu>
2018-10-04 14:48:57 -0400

    hello

--- src/BjamilLiv.java
+++ src/BjamilLiv.java
@@ -1,2 +1,10 @@
 public class BjamilLiv {
+    public static void palindrome(String str1){
+        str1.length() = int i;
+        String str2 = str1.substring(i--);
+                if(str2.compareTo(str1)=0)
+                    return true;
+                else
+                    return false;
+    }
 }
